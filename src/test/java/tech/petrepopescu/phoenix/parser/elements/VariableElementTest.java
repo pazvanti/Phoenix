2747dd78c9ccca250910cb3159200c8192c0a216
Petre Popescu <pazvanti2003@gmail.com>
2024-12-22 11:16:31 +0200

    Fix variable element inside URL not being parsed correctly

--- src/test/java/tech/petrepopescu/phoenix/parser/elements/VariableElementTest.java
+++ src/test/java/tech/petrepopescu/phoenix/parser/elements/VariableElementTest.java
@@ -3,6 +3,7 @@ package tech.petrepopescu.phoenix.parser.elements;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import tech.petrepopescu.phoenix.parser.ElementFactory;
+import tech.petrepopescu.utils.TestUtil;
 
 import java.util.List;
 import java.util.Set;
@@ -54,5 +55,19 @@ class VariableElementTest {
                 "\t\tcontentBuilder.append(\"\\n\");\n";
 
         Assertions.assertEquals(expected, element.write().toString());
+    }
+
+    @Test
+    void variableInsideUrlCall() {
+        String line = "/user/myCode/snippet/@item.getId()/edit";
+        Element element = new HtmlElement(List.of(line), 0, new ElementFactory(Set.of()), ElementFactory.DEFAULT_BUILDER_NAME);
+        element.parse("");
+
+        String expected = "\t\tcontentBuilder.append(STATIC_HTML_THISISUUID);\n" +
+                "\t\tcontentBuilder.append(Stringifier.toString(item.getId()));\n" +
+                "\t\tcontentBuilder.append(STATIC_HTML_THISISUUID);\n" +
+                "\t\tcontentBuilder.append(STATIC_HTML_THISISUUID);\n";
+
+        Assertions.assertEquals(expected, TestUtil.sanitizeResult(element.write().toString()));
     }
 }
